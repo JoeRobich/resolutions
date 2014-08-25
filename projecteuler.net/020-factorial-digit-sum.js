22eb8a4c0a8b94ec50f72fd175839505fdfeeb99
Joey Robichaud <joe.robich@gmail.com>
2014-08-25 10:15:42 -0400

    Added projecteuler answers 20-22

---
+++ projecteuler.net/020-factorial-digit-sum.js
@@ -0,0 +1,76 @@
+// http://repl.it/X23
+
+// PROBLEM //
+
+/*
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+*/
+
+// HELPERS //
+
+Array.fill=function(s,v){var a=[],i=0;while(i++<s)a.push(v);return a}
+Array.prototype.sum=function(f){f=f||function(i){return i};return this.reduce(function(s,n){return s+f(n)})}
+
+// ANSWER //
+
+function add(a, b) {
+  var aDigits = a.split('').map(Number).reverse();
+  var bDigits = b.split('').map(Number).reverse();
+  var length = Math.max(aDigits.length, bDigits.length);
+
+  var sumDigits = [];
+  var carry = 0;
+  for (var index = 0; index < length; index++) {
+    var aDigit = aDigits[index] || 0;
+    var bDigit = bDigits[index] || 0;
+
+    var sum = aDigit + bDigit + carry;
+
+    sumDigits.push(sum % 10);
+    carry = sum / 10 | 0;
+  }
+
+  if (carry)
+    sumDigits.push(carry);
+
+  return sumDigits.reverse().join('');
+}
+
+function multiply(a, b) {
+  var aDigits = a.split('').map(Number).reverse();
+  var bDigits = b.split('').map(Number).reverse();
+
+  return bDigits.reduce(function(products, bDigit, index) {
+    var productDigits = Array.fill(index, 0);
+    var carry = 0;
+    aDigits.forEach(function(aDigit) {
+        var product = aDigit * bDigit + carry;
+        productDigits.push(product % 10);
+        carry = product / 10 | 0;
+    });
+
+    if (carry)
+      productDigits.push(carry);
+
+    products.push(productDigits.reverse().join(''));
+    return products;
+  },[]).reduce(add);
+}
+
+function factorial(n) {
+  var fac = n.toString();
+
+  for (n--; n > 1; n--)
+    fac = multiply(fac, n.toString());
+
+  return fac;
+}
+
+factorial(100).split('').map(Number).sum();
+
+// 648
---
+++ projecteuler.net/021-amicable-numbers.js
@@ -0,0 +1,37 @@
+//
+
+// PROBLEM //
+
+/*
+Let d(n) be defined as the sum of proper divisors of n (numbers less than n
+which divide evenly into n).
+If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and
+each of a and b are called amicable numbers.
+
+For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55
+and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and
+142; so d(284) = 220.
+
+Evaluate the sum of all the amicable numbers under 10000.
+*/
+
+// HELPERS //
+
+Number.prototype.factors=function(){var a=[],t=this,l=1,h=t;while(l<h){if(!(t%l)){h=t/l;a.push(l);if(h!=l)a.push(h)}l++}return a}
+Array.range=function(s,e){var a=[];while(s<e)a.push(s++);return a}
+Array.prototype.sum=function(f){f=f||function(i){return i};return this.length?this.reduce(function(s,n){return s+f(n)}):0}
+
+// ANSWER //
+
+function getSumOfAmicablesTo(n) {
+  function isAmicable(a) {
+    var b = a.factors().sum() - a;
+    var c = b.factors().sum() - b;
+    return a != b && a == c;
+  }
+  return Array.range(2, n).filter(isAmicable).sum();
+}
+
+getSumOfAmicablesTo(10000);
+
+// 31626
