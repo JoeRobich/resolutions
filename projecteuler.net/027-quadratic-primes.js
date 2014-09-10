fc71ef92ea48f148b7009dce7733742748461a31
Joey Robichaud <joe.robich@gmail.com>
2014-09-10 08:34:07 -0400

    Added projecteuler answer 27

--- projecteuler.net/027-quadratic-primes.js
+++ projecteuler.net/027-quadratic-primes.js
@@ -1,5 +1,4 @@
-#! node --harmony
-"use strict";
+// http://repl.it/Ydx
 
 // PROBLEM //
 
@@ -29,11 +28,71 @@ n = 0.
 
 // HELPERS //
 
+Array.prototype.fill=function(v){var i=0,a=[];while(i++<this.length)a.push(v);return a}
+Array.range=function(s,e){var a=[];while(s<e)a.push(s++);return a}
 
 // ANSWER //
 
-var result = true;
+function negate(n) {
+  return -n;
+}
+
+function getPrimesTo(n) {
+  function getPrimeMap(n) {
+    var primeMap = new Array(n).fill(true);
+    primeMap[0]=primeMap[1]=false;
+
+    var sqrtN = Math.sqrt(n);
+    for (var i = 2; i < sqrtN; i++)
+      if (primeMap[i])
+        for (var j = i * i; j < n; j += i)
+          primeMap[j] = false;
+
+    return primeMap;
+  }
+
+  return getPrimeMap(n).reduce(function(p,f,i) {
+    if (f)
+      p.push(i);
+    return p;
+  }, []);
+}
+
+function findProductForLongestPrimeStreak(max) {
+  var primeMap = getPrimesTo((max * max * 2) + max).reduce(function(m,p) { m[p] = 1; return m}, {});
+
+  var aValues = Array.range(-(max - 1), max);
+  var bValues = getPrimesTo(max - 1);
+  bValues = bValues.map(negate).reverse().concat(bValues);
+
+  var longestStreak = 0;
+  var longestA = -1;
+  var longestB = -1;
+
+  for (var aIndex = 0; aIndex < aValues.length; aIndex++)
+  for (var bIndex = 0; bIndex < bValues.length; bIndex++) {
+    var a = aValues[aIndex];
+    var b = bValues[bIndex];
+
+    var n = 0;
+    var value = (n * n) + (n * a) + b;
+    while (primeMap[value]) {
+      n++;
+      value = (n * n) + (n * a) + b;
+    }
+
+    if (n > longestStreak) {
+      longestStreak = n;
+      longestA = a;
+      longestB = b;
+    }
+  }
+
+  return longestA * longestB;
+}
+
+var result = findProductForLongestPrimeStreak(1000);
 
 console.log(result);
 
-//
+// -59231
